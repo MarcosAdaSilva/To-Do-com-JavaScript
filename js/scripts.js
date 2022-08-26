b8126662a09bc32556c3e196c14bb4757c105f42
MarcosAdaSilva <marcaofly@gmail.com>
2022-08-26 01:14:01 -0300

    Trabalhando

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // Seleção de elementos
 const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-input");
-const todoList = document.querySelector("#todo-form");
+const todoList = document.querySelector("#todo-list");
 const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
@@ -11,7 +11,7 @@ const saveTodo = (text) => {
   const todo = document.createElement("div");
   todo.classList.add("todo");
 
-  const todoTitle = document.createComment("h3");
+  const todoTitle = document.createElement("h3");
   todoTitle.innerText = text;
   todo.appendChild(todoTitle);
 
